70eb39a1f99af1e60b95f4ece364847567f7ec4d
babayoga-00 <gitmakefit@gmail.com>
2025-08-12 14:07:57 +0530

    first

---
+++ calculator.js
@@ -0,0 +1,94 @@
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+let dot = document.getElementById("dot");
+let multiply = document.getElementById("mult");
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+let divide = document.getElementById("slash");
+let percent = document.getElementById("per");
+let entry_bar = document.getElementById("entrybar");
+let clear = document.getElementById("clear");
+let input = document.getElementById("display");
+one.addEventListener("click",() => {
+   console.log("1");
+   entrybar.innerText = "1";
+   
+});
+two.addEventListener("click",() => {
+   console.log("2");
+   entrybar.innerText += "2";
+});
+three.addEventListener("click",() => {
+   console.log("3");
+   entrybar.innerText += "3";
+});
+four.addEventListener("click",() => {
+   console.log("4");
+   entrybar.innerText += "4";
+});
+five.addEventListener("click",() => {
+   console.log("5");
+   entrybar.innerText += "5";
+});
+six.addEventListener("click",() => {
+   console.log("6");
+   entrybar.innerText += "6";
+});
+seven.addEventListener("click",() => {
+   console.log("7");
+   entrybar.innerText += "7";
+});
+eight.addEventListener("click",() => {
+   console.log("8");
+   entrybar.innerText += "8";
+});
+nine.addEventListener("click",() => {
+   console.log("9");
+   entrybar.innerText += "9";
+});
+zero.addEventListener("click",() => {
+   console.log("0");
+   entrybar.innerText += "0";
+});
+dot.addEventListener("click",() => {
+   console.log(".");
+   entrybar.innerText += ".";
+});
+multiply.addEventListener("click",() => {
+   console.log("X");
+   entrybar.innerText = "x";
+});
+divide.addEventListener("click",() => {
+   console.log("/");
+   entrybar.innerText = "/";
+});
+minus.addEventListener("click",() => {
+   console.log("-");
+   entrybar.innerText = "-";
+});
+plus.addEventListener("click",() => {
+   console.log("+");
+   entrybar.innerText = "+";
+});
+percent.addEventListener("click",() => {
+   console.log("%");
+   entrybar.innerText = "%";
+});
+clear.addEventListener("click",() => {
+   console.log("clear");
+   // entrybar.innerText = "";
+   // // entrybar.innerText = "0";
+   
+});
+
+function calculate(params) {
+   
+}
